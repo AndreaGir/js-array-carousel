8cd7a4b41d8ae17f7d1349d28584c9d99287683c
AndreaGir <girolamiandrea2000@gmail.com>
2022-02-21 23:41:32 +0100

    3

--- app.js
+++ app.js
@@ -1,46 +1,55 @@
-let immaginiCitta = [
-    'immagini/01.jpg',
-    'immagini/02.jpg',
-    'immagini/03.jpg',
-    'immagini/04.jpg',
-    'immagini/05.jpg'
+const immaginiCitta = [
+    "immagini/01.jpg",
+    "immagini/02.jpg",
+    "immagini/03.jpg",
+    "immagini/04.jpg",
+    "immagini/05.jpg"
 ];
 
 let titoliCitta = [
-    'Svezia',
-    'Svizzera',
-    'Gran Bretagna',
-    'Germania',
-    'Paradise'
+    "Svezia",
+    "Svizzera",
+    "Gran Bretagna",
+    "Germania",
+    "Paradise"
 ]
 
-const containerDiv = document.getElementById("dentro")
+const containerDiv = document.querySelector(".container")
 
-let activeElement;
+let activeElement ;
 
 let bottonedestra = document.querySelector(".bdes")
 
 let bottonesinistra = document.querySelector(".bsin")
 
 
-for (i = 0 ; i < immaginiCitta.length ; i++){
 
-    const div = document.createElement('div');
+for (let i = 0 ; i < immaginiCitta.length ; i++){
+
+    const divElement = document.createElement('div');
 
     const carta = `<h2>${titoliCitta[i]}</h2><img src="${immaginiCitta[i]}">`
 
-    containerDiv.append(div)
+    containerDiv.append(divElement)
 
-    containerDiv.innerHTML = carta
+    divElement.innerHTML = carta
 
-    if (i === 0){
-        div.classList.add("active")
+    if (i == 0){
+        divElement.classList.add("active")
         activeElement = 0
 
     } 
 
 }
 
+bottonedestra.addEventListener("click", function(){
+    
+    const currentDiv = document.getElementsByClassName("active")[0]
+    currentDiv.classList.remove("active")
+    activeElement++
+    const nextDiv = document.getElementsByClassName(titoliCitta[activeElement])[0]
+    nextDiv.classList.add("active")
+})
    
 
 
