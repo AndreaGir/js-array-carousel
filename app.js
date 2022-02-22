9da6f8c3cfc7dc6fbe5a7aa63470ec61b78df937
AndreaGir <girolamiandrea2000@gmail.com>
2022-02-22 15:31:33 +0100

    ...

--- app.js
+++ app.js
@@ -22,11 +22,15 @@ let bottonedestra = document.querySelector(".bdes")
 
 let bottonesinistra = document.querySelector(".bsin")
 
+const slides = [];
+
 
 
 for (let i = 0 ; i < immaginiCitta.length ; i++){
 
     const divElement = document.createElement('div');
+
+    divElement.classList.add('slide')
 
     const carta = `<h2>${titoliCitta[i]}</h2><img src="${immaginiCitta[i]}">`
 
@@ -40,17 +44,29 @@ for (let i = 0 ; i < immaginiCitta.length ; i++){
 
     } 
 
+    slides.push(divElement)
+
 }
+
+console.log(slides)
 
 bottonedestra.addEventListener("click", function(){
     
-    const currentDiv = document.getElementsByClassName("active")[0]
+    const currentDiv = slides[activeElement]
     currentDiv.classList.remove("active")
     activeElement++
-    const nextDiv = document.getElementsByClassName(titoliCitta[activeElement])[0]
+    const nextDiv = slides[activeElement]
     nextDiv.classList.add("active")
 })
    
+bottonesinistra.addEventListener("click", function(){
+    
+    const currentDiv = slides[activeElement]
+    currentDiv.classList.remove("active")
+    activeElement--
+    const nextDiv = slides[activeElement]
+    nextDiv.classList.add("active")
+})
 
 
 
